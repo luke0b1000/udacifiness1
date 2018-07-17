bb16ba75b47f0702b49285a17ad0f4f99098242c
luke0b1000 <luke0b1000@gmail.com>
2018-07-17 00:01:34 -0400

    2x3x4 Added icons

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import AddEntry from "./Components/AddEntry";
 
 export default class App extends React.Component {
--- Components/AddEntry.js
+++ Components/AddEntry.js
@@ -1,9 +1,71 @@
 import React, { Component } from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import { getMetricMetaInfo } from "../utils/helpers";
+import UdaciSliders from "./UdaciSliders";
+import UdaciSteppers from "./UdaciSteppers";
+import { Slider } from "react-native-gesture-handler";
 
 export default class AddEntry extends Component {
+    state = {
+        run: 0,
+        bike: 0,
+        swim: 0,
+        sleep: 0,
+        eat: 0
+    };
+    increment = metric => {
+        const { max, step } = getMetricMetaInfo(metric);
+
+        this.setState(state => {
+            const count = state[metric] + step;
+            return {
+                [metric]: count > max ? max : count
+            };
+        });
+    };
+    decrement = metric => {
+        this.setState(state => {
+            const count = state[metric] - getMetricMetaInfo(metric).step;
+            return {
+                [metric]: count < 0 ? 0 : count
+            };
+        });
+    };
+    slide = (metric, value) => {
+        this.setState(() => ({
+            [metric]: value
+        }));
+    };
     render() {
-        return <View>{getMetricMetaInfo("bike").getIcon()}</View>;
+        const metaInfo = getMetricMetaInfo();
+        return (
+            <View>
+                // key is bike, run... as it loops // rest is an Object
+                {Object.keys(metaInfo).map(key => {
+                    const { getIcon, type, ...rest } = metaInfo[key];
+                    const value = this.state[key];
+                    return (
+                        <View key={key}>
+                            {getIcon()}
+                            {type === "sliders" ? (
+                                <UdaciSliders
+                                    value={value}
+                                    onChange={value => this.slide(key, value)}
+                                    {...rest}
+                                />
+                            ) : (
+                                <UdaciSteppers
+                                    value={value}
+                                    onIncrement={() => this.increment(key)}
+                                    onDecrement={() => this.decrement(key)}
+                                    {...rest}
+                                />
+                            )}
+                            }
+                        </View>
+                    );
+                })}
+            </View>
+        );
     }
 }
