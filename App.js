530ebece9ce8dd5c7b04d891060139c379f731eb
luke0b1000 <luke0b1000@gmail.com>
2018-07-23 20:43:47 -0400

    3x4x7 Adding bare history

--- App.js
+++ App.js
@@ -4,13 +4,14 @@ import AddEntry from "./Components/AddEntry";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers";
+import History from "./Components/History";
 
 export default class App extends React.Component {
     render() {
         return (
             <Provider store={createStore(reducer)}>
-                <View style={{flex:1}}>
-                    <AddEntry />
+                <View style={{ flex: 1 }}>
+                    <History />
                 </View>
             </Provider>
         );
--- utils/api.js
+++ utils/api.js
@@ -1,5 +1,11 @@
 import { AsyncStorage } from "react-native";
-import { CALENDAR_STORAGE_KEY } from "./_calendar";
+import { CALENDAR_STORAGE_KEY, formatCalendarResults } from "./_calendar";
+
+export function fetchCalendarResults() {
+    return AsyncStorage.getItem(CALENDAR_STORAGE_KEY).then(
+        formatCalendarResults
+    );
+}
 
 export function submitEntry({ entry, key }) {
     return AsyncStorage.mergeItem(
@@ -11,11 +17,10 @@ export function submitEntry({ entry, key }) {
 }
 
 export function removeEntry(key) {
-    return AsyncStorage.getItem(CALENDAR_STORAGE_KEY)
-        .then((results)=> {
-            const data = JSON.parse(results)
-            data[key] = undefined
-            delete data[key] // delete the property i Think
-            AsyncStorage.setItem(CALENDAR_STORAGE_KEY, JSON.stringify(data))
-        })
+    return AsyncStorage.getItem(CALENDAR_STORAGE_KEY).then(results => {
+        const data = JSON.parse(results);
+        data[key] = undefined;
+        delete data[key]; // delete the property i Think
+        AsyncStorage.setItem(CALENDAR_STORAGE_KEY, JSON.stringify(data));
+    });
 }
